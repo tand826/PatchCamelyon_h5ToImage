b331e7ef8992066f2f99373f1f2a343baa510916
takumiando <takumiando@m.u-tokyo.ac.jp>
2019-08-28 15:11:27 +0900

    fixed

--- patch2img.py
+++ patch2img.py
@@ -14,15 +14,15 @@ def main():
         print(f"[{phase} : X]")
         file_x = h5py.File(f"camelyonpatch_level_2_split_{phase}_x.h5")
         data_x = file_x['x']
-        for idx, img in tqdm(enumerate(data_x)):
-            cv2.imwrite(f"{phase}/x/{idx:05}.png", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+        #for idx, img in tqdm(enumerate(data_x)):
+        #    cv2.imwrite(f"{phase}/x/{idx:06}.png", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
         print(f"[{phase} : Y]")
         file_y = h5py.File(f"camelyonpatch_level_2_split_{phase}_y.h5")
         data_y = file_y['y']
         out = np.zeros(data_y.shape, dtype=np.uint8)
         data_y.read_direct(out)
-        pd.DataFrame(out.ravel()).to_csv(f"{phase}/y".csv)
+        pd.DataFrame(out.ravel()).to_csv(f"{phase}/y.csv", header=None, index=None)
 
 
 if __name__ == '__main__':
