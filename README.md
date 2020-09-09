4e7ae2c8efe559e8889221cdf5262ee63f6fbd0d
Takumi Ando <takumi.ando826@gmail.com>
2020-09-09 11:19:50 +0900

    added md5sum hashes for troubleshooting

--- README.md
+++ README.md
@@ -10,11 +10,16 @@ python patch2img.py
 - https://github.com/basveeling/pcam
 - download script
 	- `./download.sh`
+- check hash
+	- `md5sum -c sums.txt`
 
 # Requirements
 ```
 pip install -r requirements.txt
 ```
+
+# Troubleshoot
+- If you have troubles in files, please try to chech hashes by `md5sum -c sums.txt`.
 
 # Notice
 - Now (Aug.31.2019) the MD5 Checksum in the dataset page is not correct. "camelyonpatch_level_2_split_valid_x.h5.gz" and "camelyonpatch_level_2_split_test_x.h5.gz" is replaced.
--- download.sh
+++ download.sh
@@ -2,9 +2,13 @@ for phase in train valid test
 do
 curl -O -C - https://zenodo.org/record/2546921/files/camelyonpatch_level_2_split_${phase}_x.h5.gz
 gunzip camelyonpatch_level_2_split_${phase}_x.h5.gz
-rm camelyonpatch_level_2_split_${phase}_x.h5.gz
 curl -O -C - https://zenodo.org/record/2546921/files/camelyonpatch_level_2_split_${phase}_y.h5.gz
 gunzip camelyonpatch_level_2_split_${phase}_y.h5.gz
-rm camelyonpatch_level_2_split_${phase}_x.h5.gz
 curl -O -C - https://zenodo.org/record/2546921/files/camelyonpatch_level_2_split_${phase}_meta.csv
+done
+
+for phase in train valid test
+do
+    echo "Checking hash"
+    md5sum -c sums.txt
 done
